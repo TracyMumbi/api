59d63574ea17fcff83527b891aa59aa72e554f80
Tracy Mumbi <tracychiuri@gmail.com>
2023-07-19 23:55:41 +0300

    added candidates table

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@ end
       gender: Faker::Gender.binary_type,
     )
   end
+  candidate = Candidate.create!(reg_no: rand(1..10), student_name: Faker::Internet.username, student_email: Faker::Internet.email, voting_phone_number: 071234567, department: "Pau"
+  )
