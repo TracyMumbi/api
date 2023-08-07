2ea8537a2e98f0f0919ab975d6e9ff3cb712cd5f
Tracy Mumbi <tracychiuri@gmail.com>
2023-08-07 23:12:03 +0300

    added more candidatex

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,10 @@ end
       gender: Faker::Gender.binary_type,
     )
   end
+  20.times do
   candidate = Candidate.create!(reg_no: rand(1..10), student_name: Faker::Internet.username, student_email: Faker::Internet.email, voting_phone_number: 071234567, department: "Pau"
   )
+  end
 
   20.times do
     vote = Vote.create!(VVPAT_ref: "#359q950450945", condidate_selected: Faker::Name.name, actual_vote_count: rand(1..10))
